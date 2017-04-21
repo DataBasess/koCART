b6a84f36900d3fccfba379fa64524899e930e48a
DataBasess <anusondd@gmail.com>
2017-04-21 13:49:11 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://c2store.azurewebsites.net';
-$config['admin_url']	= 'http://c2store.azurewebsites.net/admin/';
+$config['base_url']	= '';
+$config['admin_url']	= '';
 /*
 $config['base_url']	= 'http://c2store.azurewebsites.net';
 $config['admin_url']	= 'http://c2store.azurewebsites.net/admin/';
