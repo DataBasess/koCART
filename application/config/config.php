2b46c230de6f8bdb462b851a600227655e8a2496
DataBasess <anusondd@gmail.com>
2017-04-21 13:30:21 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://jolyjokerz.com/';
-$config['admin_url']	= 'http://jolyjokerz.com/admin/';
+$config['base_url']	= 'http://c2store.azurewebsites.net';
+$config['admin_url']	= 'http://c2store.azurewebsites.net/admin/';
 /*
 |--------------------------------------------------------------------------
 | Index File
